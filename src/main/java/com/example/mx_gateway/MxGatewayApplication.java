3a98e4088efcc5d375f0f7be122e547b4361498f
cycdc98 <cycdc98@163.com>
2026-07-03 23:50:57 +0800

    init

---
+++ src/main/java/com/example/mx_gateway/MxGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.example.mx_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class MxGatewayApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MxGatewayApplication.class, args);
+	}
+
+}
